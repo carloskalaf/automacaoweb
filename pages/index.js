f2fa295fe36735e76b23c7cf554d9dfc22c4350f
Carlos Kalaf <carlos.kalaf@hotmail.com>
2021-04-14 00:09:53 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 
 function home(){
     return  (
-    <div>Automação Web teste</div>
+    <div>
+    
+    Automação Web teste
+        <div>
+            <form method= "post">
+                <input type="text"  name="login"/>
+                <input type="password" name="password"/>
+                <input type="submit" name="acao" value="logar"/>
+            </form>
+        </div>
+
+    </div>
     )
 
 }
